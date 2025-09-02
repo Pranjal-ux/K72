4570070541cc05f5ac68d4288dda7994ff61c243
Pranjal-ux <pranjal2shukla@gmail.com>
2025-09-02 10:38:01 +0530

    #Agence-imageHalfDone

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import React, { useRef } from "react";
 
 const Agence = () => {
-  return (
-    <div>Agence</div>
-  )
-}
+  gsap.registerPlugin(ScrollTrigger);
+  const imgDivRef = useRef(null);
+  useGSAP(() => {
+    gsap.to(imgDivRef.current, {
+      scrollTrigger: {
+        trigger: imgDivRef.current,
+        start: "top 15%",
+        end: "top -112%",
+        pin: true,
+        scrub: true,
+      },
+    });
+  });
 
-export default Agence
+  return (
+    <div>
+      <div className="section-1">
+        <div
+          ref={imgDivRef}
+          className=" absolute top-[15vh] left-[33vw]  h-[50vh] w-[15vw] overflow-hidden"
+        >
+          <img
+            className="rounded-3xl h-full w-full object-cover"
+            src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg"
+            alt="P-image"
+          />
+        </div>
+        <div className="font-[Font02] relative">
+          <div className="mt-[55vh]  ">
+            <h1 className="text-[17vw] uppercase text-center leading-[15vw] ">
+              Soixan7e
+              <br /> Douze
+            </h1>
+          </div>
+          <div className="pl-[50%]">
+            <p className="text-5xl indent-[10vw] leading-[3vw]">
+              Notre curiosité nourrit notre créativité. On reste humbles et on
+              dit non aux gros egos, même le vôtre. Une marque est vivante. Elle
+              a des valeurs, une personnalité, une histoire. Si on oublie ça, on
+              peut faire de bons chiffres à court terme, mais on la tue à long
+              terme. C’est pour ça qu’on s’engage à donner de la perspective,
+              pour bâtir des marques influentes.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="section-2 h-screen"></div>
+    </div>
+  );
+};
+
+export default Agence;

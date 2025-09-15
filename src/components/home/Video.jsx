0a13aa38a0039039288586ebbfb21d9b8cc10eb1
Pranjal-ux <pranjal2shukla@gmail.com>
2025-09-15 17:15:17 +0530

    #FinalChange

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -8,7 +8,7 @@ const Video = () => {
         autoPlay
         loop
         muted
-        src="https://download-video-ak.vimeocdn.com/v3-1/playback/a278bb9c-996c-4760-8ca1-dd0abff308f7/83c745cf?__token__=st=1757851441~exp=1757855041~acl=%2Fv3-1%2Fplayback%2Fa278bb9c-996c-4760-8ca1-dd0abff308f7%2F83c745cf%2A~hmac=a71e7f036e0996d937ac3d5f0a3229c227e881e295bf3e7e9229ba7f0492e6e4&r=dXMtd2VzdDE%3D"
+        src="../../../public/Video/video.mp4"
       ></video>
     </div>
   );

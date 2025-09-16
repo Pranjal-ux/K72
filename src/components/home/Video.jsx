72334c28443c0adeb64eafe2ae7430e6bd02bf23
Pranjal-ux <pranjal2shukla@gmail.com>
2025-09-16 19:04:32 +0530

    .

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,12 +3,15 @@ import React from "react";
 const Video = () => {
   return (
     <div className="h-full w-full">
-      <iframe
+      <video
         className="h-full w-full object-cover"
-        src="https://download-video-ak.vimeocdn.com/v3-1/playback/a278bb9c-996c-4760-8ca1-dd0abff308f7/83c745cf?__token__=st=1758028484~exp=1758032084~acl=%2Fv3-1%2Fplayback%2Fa278bb9c-996c-4760-8ca1-dd0abff308f7%2F83c745cf%2A~hmac=4004468eaee4c7a9e2714da333b780f5ce38df4e3fbcd82a86ef337925a21b48&r=dXMtd2VzdDE%3D"
-        frameborder="0"
-        allowfullscreen
-      ></iframe>
+        autoPlay
+        loop
+        muted
+        playsInline
+        controls={false}
+        src="public/Video/video.mp4"
+      ></video>
     </div>
   );
 };

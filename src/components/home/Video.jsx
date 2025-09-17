83acca486e910ddbbfe2c9ad81747d50d12da315
Pranjal-ux <pranjal2shukla@gmail.com>
2025-09-17 20:22:43 +0530

    mm

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,13 +3,11 @@ import React from "react";
 const Video = () => {
   return (
     <div className="h-full w-full">
-      <video
-        className="h-full w-full object-cover"
-        autoPlay
-        loop
-        muted
-        src="../../../public/video/video.mp4"
-      ></video>
+      <video className="h-full w-full object-cover" autoPlay loop muted>
+        <source src="/video/video.mp4" type="video/mp4" />
+        {/* fallback content if video not supported */}
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 };

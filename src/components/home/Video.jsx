4fd943a8c435e77cf4e25d218d6450c2e835cc7a
Pranjal-ux <pranjal2shukla@gmail.com>
2025-09-18 19:50:46 +0530

    v

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 const Video = () => {
   return (
-    <div className="h-full w-full ">
-      <img
-        className="h-full w-full object-cover"
-        src="https://imgs.search.brave.com/W2m1qCQ6eyT9xcNVeLrqQo_IGbVx4BzyiwLz1XjFi_s/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTM2/ODY0NDE0NC9waG90/by9wbGFuZXQtZWFy/dGgtc2Vlbi1mcm9t/LXNwYWNlLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz1fbFY5/ZzhLNzE4cTkzZTRx/djFJbng1TmJubGsy/THFNeXFlMWFPNFNH/cV93PQ"
-        alt=""
+    <div className="h-full w-full">
+      <video
+        src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1758205168~exp=1758208768~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=71f3c3a55313d8c6d567dae5a00d14ab77ec3fc6259d4d9e6d05036c095b6a08&r=dXMtZWFzdDE%3D"
+        autoPlay
+        muted
+        loop
+        playsInline
+        className=" w-full h-full object-cover"
       />
     </div>
   );

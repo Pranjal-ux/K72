f695380bf9c6dc0482af368a326e3fe7a78f10f2
Pranjal-ux <pranjal2shukla@gmail.com>
2025-09-18 19:43:17 +0530

    ss

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Video = () => {
   return (
-    <div className="h-full w-full bg-red-400">
+    <div className="h-full w-full ">
       <img
         className="h-full w-full object-cover"
         src="/public/video/img01.jpg"

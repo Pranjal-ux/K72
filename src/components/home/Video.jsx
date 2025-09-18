ee9ca0b7f9ebb67e450576275732f2ff9caa26a9
Pranjal-ux <pranjal2shukla@gmail.com>
2025-09-18 19:47:20 +0530

    x

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -5,7 +5,7 @@ const Video = () => {
     <div className="h-full w-full ">
       <img
         className="h-full w-full object-cover"
-        src="https://imgs.search.brave.com/t-Ir_G9JRe6CPI7Arsv73UxK4SuDVec96PFci5f8-Tc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDkxMTY2/MjcuanBnsw"
+        src="https://imgs.search.brave.com/W2m1qCQ6eyT9xcNVeLrqQo_IGbVx4BzyiwLz1XjFi_s/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTM2/ODY0NDE0NC9waG90/by9wbGFuZXQtZWFy/dGgtc2Vlbi1mcm9t/LXNwYWNlLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz1fbFY5/ZzhLNzE4cTkzZTRx/djFJbng1TmJubGsy/THFNeXFlMWFPNFNH/cV93PQ"
         alt=""
       />
     </div>

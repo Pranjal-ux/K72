49796f1a6114df01ea536222d6d5aa058c4ea257
Pranjal-ux <pranjal2shukla@gmail.com>
2025-09-18 19:44:48 +0530

    s

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -5,7 +5,7 @@ const Video = () => {
     <div className="h-full w-full ">
       <img
         className="h-full w-full object-cover"
-        src="/public/video/img01.jpg"
+        src="https://imgs.search.brave.com/t-Ir_G9JRe6CPI7Arsv73UxK4SuDVec96PFci5f8-Tc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDkxMTY2/MjcuanBnsw"
         alt=""
       />
     </div>

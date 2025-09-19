21438cb0f41e60eb5d9eaa7c60d9fcd3045a3918
Pranjal-ux <pranjal2shukla@gmail.com>
2025-09-19 09:49:29 +0530

    imageAdd

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,13 +3,10 @@ import React from "react";
 const Video = () => {
   return (
     <div className="h-full w-full">
-      <video
-        src="/public/video/video.mp4"
-        autoPlay
-        muted
-        loop
-        playsInline
-        className=" w-full h-full object-cover"
+      <img
+        className="h-full w-full"
+        src="https://imgs.search.brave.com/B2NCG-wvQFxYnXr6e7vaeB__N835qVfE8bESwvL_rzM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvaGQvZWFy/dGgtc3VyZmFjZS00/ay1zcGFjZS1ic2hl/cDdhNjY0MzFzNXVr/LmpwZw"
+        alt=""
       />
     </div>
   );

f0d70b965e95c801da60788355f84ff7fb9c9994
Pranjal-ux <pranjal2shukla@gmail.com>
2026-02-07 10:17:03 +0530

    #Changes

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,11 +3,7 @@ import React from "react";
 const Video = () => {
   return (
     <div className="h-full w-full">
-      <img
-        className="h-full w-full"
-        src="https://imgs.search.brave.com/B2NCG-wvQFxYnXr6e7vaeB__N835qVfE8bESwvL_rzM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvaGQvZWFy/dGgtc3VyZmFjZS00/ay1zcGFjZS1ic2hl/cDdhNjY0MzFzNXVr/LmpwZw"
-        alt=""
-      />
+      <video src="../../../video/video.mp4" autoPlay muted loop></video>
     </div>
   );
 };
